cedcb8aa552e7120df2e7a23bd877a569f36e7e0
Michael Chan <michael@innova-global.com>
2018-03-07 02:22:08 +0900

    Build - finished img, mp3, tag | next: continue

--- main.js
+++ main.js
@@ -1,29 +1,46 @@
-const Apify = require('apify');
-const typeCheck = require('type-check').typeCheck;
+const Apify = require('apify')
+const typeCheck = require('type-check').typeCheck
 
 // Definition of the input
 const INPUT_TYPE = `{
-    message: Maybe String,
-}`;
+    success: Boolean,
+    type: String,
+    data: Array,
+}`
 
 Apify.main(async () => {
     // Fetch the input and check it has a valid format
     // You don't need to check the input, but it's a good practice.
-    const input = await Apify.getValue('INPUT');
+    const input = await Apify.getValue('INPUT')
     if (!typeCheck(INPUT_TYPE, input)) {
-        console.log('Expected input:');
-        console.log(INPUT_TYPE);
-        console.log('Received input:');
-        console.dir(input);
-        throw new Error('Received invalid input');
+        console.log('Expected input:')
+        console.log(INPUT_TYPE)
+        console.log('Received input:')
+        console.dir(input)
+        throw new Error('Received invalid input')
     }
 
+    let result = ''
+    input.data.forEach(item => {
+        if (input.type === 'img') {
+            result += `<img class='image' src='${item.value}' /> `
+        }
+        else if (input.type === 'mp3') {
+            result += `[sound:${item.value}] `
+        }
+        else if (input.type === 'tag') {
+            result += `${item.value}, `
+        }
+    })
+
     // Here's the place for your magic...
-    console.log(`Input message: ${input.message}`);
+    console.log(`Input type: ${input.type}`)
 
     // Store the output
     const output = {
-        message: `${input.message} Hello my friend!`
-    };
+        input,
+        data: result
+    }
+    console.log('output', output)
     await Apify.setValue('OUTPUT', output)
-});
+})
